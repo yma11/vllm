2c17c5c0447c7a08692c2c94156192cfd4be1b82
fengqing.lu <fengqing.lu@intel.com>
2024-09-04 00:57:21 -0700

    enable lora on xpu

--- vllm/lora/worker_manager.py
+++ vllm/lora/worker_manager.py
@@ -75,6 +75,7 @@ class WorkerLoRAManager(AbstractWorkerManager):
             lora_config=self.lora_config,
             device=self.device,
             lora_manager_cls=self._manager_cls,
+            device=self.device
         )
         self._adapter_manager = lora_manager
         return lora_manager.model
@@ -198,6 +199,7 @@ class LRUCacheWorkerLoRAManager(WorkerLoRAManager):
             lora_config=self.lora_config,
             device=self.device,
             max_num_batched_tokens=self.max_num_batched_tokens,
+            device=self.device
         )
         self._adapter_manager = lora_manager
         return lora_manager.model
