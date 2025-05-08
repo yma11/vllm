9d7e301f928d47abb634c3f790da0ec83123f085
ChaoJun Zhang <chzhang@habana.ai>
2025-05-08 10:35:58 +0000

    remove bf16 support check. we assume all intel gpu should support it.

--- vllm/platforms/xpu.py
+++ vllm/platforms/xpu.py
@@ -89,17 +89,6 @@ class XPUPlatform(Platform):
 
         # check and update model config
         model_config = vllm_config.model_config
-        '''
-        if model_config.dtype == torch.bfloat16:
-            bf16_supported = cls.device_support_bf16()
-            if not bf16_supported:
-                logger.warning(
-                    "bfloat16 is only supported on Intel Data Center GPU, "
-                    "Intel Arc GPU is not supported yet. Your device is %s,"
-                    " which is not supported. will fallback to float16",
-                    cls.get_device_name())
-                model_config.dtype = torch.float16
-        '''
         if not model_config.enforce_eager:
             logger.warning(
                 "CUDA graph is not supported on XPU, fallback to the eager "
@@ -159,26 +148,9 @@ class XPUPlatform(Platform):
         return torch.float8_e5m2
 
     @classmethod
-    def device_support_bf16(cls) -> bool:
-        device_name = cls.get_device_name().lower()
-        if cls.is_client_gpu():
-            return False
-        elif cls.is_data_center_gpu():
-            return True
-        else:
-            logger.warning("Unknown device name %s, always use float16",
-                           device_name)
-            return False
-
-    @classmethod
     def is_data_center_gpu(cls) -> bool:
         device_name = cls.get_device_name().lower()
         return device_name.count("data center gpu") > 0
-
-    @classmethod
-    def is_client_gpu(cls) -> bool:
-        device_name = cls.get_device_name().lower()
-        return device_name.count("arc") > 0
 
     @classmethod
     def get_device_communicator_cls(cls) -> str:
