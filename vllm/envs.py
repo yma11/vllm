13b175659cfe39e216d51dac745a656083f9e3a1
yan <yanma1@habana.ai>
2025-05-20 05:27:11 +0000

    add VLLM_XPU_FP8_DTYPE env for fp8 online quantization
    Signed-off-by: yan <yan.ma@intel.com>
    
    Signed-off-by: Yan Ma <yan.ma@intel.com>

--- vllm/envs.py
+++ vllm/envs.py
@@ -219,6 +219,7 @@ if TYPE_CHECKING:
     VLLM_USE_FBGEMM: bool = False
     VLLM_GC_DEBUG: str = ""
     VLLM_DISABLE_SHARED_EXPERTS_STREAM: bool = False
+    VLLM_XPU_FP8_DTYPE: str = "e5m2"
 
 
 def get_default_cache_root():
@@ -1442,6 +1443,8 @@ environment_variables: dict[str, Callable[[], Any]] = {
     "VLLM_DISABLE_SHARED_EXPERTS_STREAM": lambda: os.getenv(
         "VLLM_DISABLE_SHARED_EXPERTS_STREAM", False
     ),
+    # fp8 dtype for XPU platform
+    "VLLM_XPU_FP8_DTYPE": lambda: os.environ.get("VLLM_XPU_FP8_DTYPE", "e5m2"),
 }
 
 # --8<-- [end:env-vars-definition]
