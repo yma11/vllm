554ff47327fe901bcccf26f91e4bc07d20b5368a
dbyoung18 <yang5.yang@intel.com>
2025-05-20 05:43:05 +0000

    fix(ipex_attn): fix get num_tokens
    Signed-off-by: dbyoung18 <yang5.yang@intel.com>

--- vllm/attention/backends/ipex_attn.py
+++ vllm/attention/backends/ipex_attn.py
@@ -195,7 +195,7 @@ class IpexAttnBackendImpl(AttentionImpl[IpexAttnMetadata]):
             shape = [num_tokens, num_heads * head_size]
         """
         assert layer._k_scale_float == 1.0 and layer._v_scale_float == 1.0
-        num_tokens, hidden_size = query.shape
+        num_tokens = query.shape[0]
         # Reshape the query, key, and value tensors.
         query = query.view(-1, self.num_heads, self.head_size)
         key = key.view(-1, self.num_kv_heads, self.head_size)
