87b1769984cfef9330dad985eb7b547f4bbe0cab
Kunshang Ji <kunshang.ji@intel.com>
2025-08-29 20:50:58 +0800

    try remove ipex
    Signed-off-by: Kunshang Ji <kunshang.ji@intel.com>

--- vllm/platforms/__init__.py
+++ vllm/platforms/__init__.py
@@ -127,7 +127,7 @@ def xpu_platform_plugin() -> Optional[str]:
     logger.debug("Checking if XPU platform is available.")
     try:
         # installed IPEX if the machine has XPUs.
-        import intel_extension_for_pytorch  # noqa: F401
+        # import intel_extension_for_pytorch  # noqa: F401
         import torch
         if supports_xccl():
             dist_backend = "xccl"
