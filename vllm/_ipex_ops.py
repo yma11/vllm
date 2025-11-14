a94077646e04159c08f9065f3d6164024960e983
Qiming Zhang <qiming1.zhang@intel.com>
2025-11-13 17:10:07 -0800

    remove contiguous of k & v cache (#2)
    * remove contiguous
    
    Signed-off-by: mayuyuace <qiming1.zhang@intel.com>
    
    * remove comment
    
    Signed-off-by: mayuyuace <qiming1.zhang@intel.com>
    
    ---------
    
    Signed-off-by: mayuyuace <qiming1.zhang@intel.com>

--- vllm/_ipex_ops.py
+++ vllm/_ipex_ops.py
@@ -334,14 +334,11 @@ class ipex_ops:
             )
             return out
         else:
-            # TODO：IPEX chunk prefill only support contiguous k,v
-            # The kernel need support incontiguous k, v
-            # And this causes mamba spec model low gpu-util
             return ipex.llm.modules.PagedAttention.flash_attn_varlen_func(
                 out,
                 q.contiguous(),
-                k.contiguous(),
-                v.contiguous(),
+                k,
+                v,
                 cu_seqlens_q,
                 seqused_k,
                 max_seqlen_q,
