cec181e91a4b7a631c9f4a9649c3e3fdf04b10b4
Yan Ma <yan.ma@intel.com>
2026-01-27 06:29:53 +0000

    Revert "offload weights to cpu before fp8 online quant (#225)"
    This reverts commit fc5a0a6f07bd2217d90da3d5374c2ed39b9d9660.

--- vllm/model_executor/layers/quantization/fp8.py
+++ vllm/model_executor/layers/quantization/fp8.py
@@ -417,15 +417,11 @@ class Fp8LinearMethod(LinearMethodBase):
                 return res
 
             # For non-serialized checkpoints, use original dtype
-            # Force offloading weights to cpu if VLLM_OFFLOAD_WEIGHTS_BEFORE_QUANT
-            # enabled, otherwise use original device config which can be gpu or cpu
-            # (may happen when cpu_offload_gb > 0)
             weight = ModelWeightParameter(
                 data=torch.empty(
                     output_size_per_partition,
                     input_size_per_partition,
                     dtype=params_dtype,
-                    device="cpu" if envs.VLLM_OFFLOAD_WEIGHTS_BEFORE_QUANT else None,
                 ),
                 input_dim=1,
                 output_dim=0,
@@ -719,7 +715,6 @@ class Fp8MoEMethod(FusedMoEMethodBase):
                 2 * intermediate_size_per_partition,
                 hidden_size,
                 dtype=params_dtype,
-                device="cpu" if envs.VLLM_OFFLOAD_WEIGHTS_BEFORE_QUANT else None,
             ),
             requires_grad=False,
         )
@@ -732,7 +727,6 @@ class Fp8MoEMethod(FusedMoEMethodBase):
                 hidden_size,
                 intermediate_size_per_partition,
                 dtype=params_dtype,
-                device="cpu" if envs.VLLM_OFFLOAD_WEIGHTS_BEFORE_QUANT else None,
             ),
             requires_grad=False,
         )
