a6d07e2d00445b8d4d9653ae1532dd9c2b33a5c5
Yan Ma <yan.ma@intel.com>
2026-05-15 04:59:32 +0000

    cherry-pick kv cache offloading - 36423
    Signed-off-by: Yan Ma <yan.ma@intel.com>

--- vllm/v1/kv_offload/cpu/spec.py
+++ vllm/v1/kv_offload/cpu/spec.py
@@ -86,7 +86,7 @@ class CPUOffloadingSpec(OffloadingSpec):
         self, kv_caches: CanonicalKVCaches
     ) -> Iterator[tuple[type[LoadStoreSpec], type[LoadStoreSpec], OffloadingHandler]]:
         if not self._handlers:
-            if not current_platform.is_cuda_alike():
+            if not (current_platform.is_cuda_alike() or current_platform.is_xpu()):
                 raise Exception(
                     "CPU Offloading is currently only supported on CUDA-alike GPUs"
                 )
